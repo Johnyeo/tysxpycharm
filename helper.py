6da06cb84d76aae10836ed32f6bb95d2c23cbfc8
zhangyao <helloenola@gmail.com>
2017-11-20 11:16:34 +0800

    Revert "tysx"
    This reverts commit d94446520b008dbade7a559110dd3db32aab9756.

--- helper.py
+++
@@ -1,8 +0,0 @@
-# coding:utf-8
-import os
-
-CURRENT_PATH = os.getcwd()
-PROJECT_DIR = os.path.abspath(os.path.join(CURRENT_PATH, os.pardir))
-UPLOAD_EXE_DIR = CURRENT_PATH+"\\"
-UPLOAD_EXE_DIR2 = UPLOAD_EXE_DIR.replace('\\','/')
-print(UPLOAD_EXE_DIR)
--- tysxunittest/autoit_scripts/word_path.au3
+++
@@ -1,2 +0,0 @@
-MsgBox(0, "当前工作目录：", @WorkingDir & '\hello');
-MsgBox(0, "当前工作目录：", @ScriptDir);
